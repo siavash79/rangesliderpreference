d9c7475315d78ee795fe085540ad66bbf8e12b6e
Siavash <25217714+siavash79@users.noreply.github.com>
2025-03-06 08:38:54 +0330

    RangeSliderPreference: dependency and build script update

---
+++ build.gradle.kts
@@ -0,0 +1,35 @@
+plugins {
+    id("com.android.library")
+}
+
+android {
+    namespace = "sh.siava.rangesliderpreference"
+
+    compileSdk = 35
+
+    defaultConfig {
+        minSdk = 22
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = true
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+}
+
+dependencies {
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("com.google.android.material:material:1.12.0")
+    implementation("androidx.preference:preference:1.2.1")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
+}
